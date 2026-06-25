0e2d3868dde082fdab41c3e9a5db2d0f4760c5b5
dositadi <akindivine587@gmail.com>
2026-06-25 11:05:20 +0100

    setting user update working now

--- internal/handlers/posthandler/authpost/update_profile_hander.go
+++ internal/handlers/posthandler/authpost/update_profile_hander.go
@@ -18,130 +18,12 @@ func (a *Auth) UpdateProfileHandler(w http.ResponseWriter, r *http.Request) {
 	currentPass := r.FormValue(utils.PASSWORD_KEY)
 	newPass := r.FormValue(utils.NEW_PASSWORD_KEY)
 	confirmPass := r.FormValue(utils.CONFIRM_PASS_KEY)
-
 	user := a.getUser(r)
 	page := authservice.New(w, a.embedded, a.logger, r, a.usermodel)
 
-	var updateUser data.UpdateUser
-
-	if currentPass != "" {
-		userPass, err := a.usermodel.GetWithEmail(user.Email)
-		if err != nil {
-			e := helper.WrapError("User fetch error", err)
-			a.logger.PrintError(e.Error(), map[string]string{
-				"Source": sourcePr,
-			})
-
-			if err = page.RenderInfo(authservice.InfoForm, authservice.Info{
-				Title:   "Something wrong happened.",
-				Message: "Kindly try again.",
-			}, true); err != nil {
-				a.logger.PrintError(err.Error(), map[string]string{
-					"Source": sourcePr,
-				})
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-			return
-		}
-
-		if err = a.compareHashedPassword(userPass.HashedPassword, []byte(currentPass)); err != nil {
-			e := helper.WrapError("The current password is not correct", err)
-			a.logger.PrintError(e.Error(), map[string]string{
-				"Source": sourcePr,
-			})
-
-			if err = page.RenderInfo(authservice.InfoForm, authservice.Info{
-				Title:   "Password mismatch.",
-				Message: "The current password you provided is not correct.",
-			}, true); err != nil {
-				a.logger.PrintError(err.Error(), map[string]string{
-					"Source": sourcePr,
-				})
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
-			return
-		}
-
-		if newPass == "" {
-			e := "The new password field cannot be empty"
-			a.logger.PrintError(e, map[string]string{
-				"Source": sourcePr,
-			})
-			if err = page.RenderInfo(authservice.InfoForm, authservice.Info{
-				Title:   "Empty field.",
-				Message: "The new password field cannot be empty.",
-			}, true); err != nil {
-				a.logger.PrintError(err.Error(), map[string]string{
-					"Source": sourcePr,
-				})
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
-			return
-		}
-
-		if confirmPass == "" {
-			e := "The confirm password field cannot be empty"
-			a.logger.PrintError(e, map[string]string{
-				"Source": sourcePr,
-			})
-			if err = page.RenderInfo(authservice.InfoForm, authservice.Info{
-				Title:   "Empty field.",
-				Message: "The confirm password field cannot be empty.",
-			}, true); err != nil {
-				a.logger.PrintError(err.Error(), map[string]string{
-					"Source": sourcePr,
-				})
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
-			return
-		}
-
-		if newPass != confirmPass {
-			e := "Password mismatch. New password and confirm password do not match"
-			a.logger.PrintError(e, map[string]string{
-				"Source": sourcePr,
-			})
-			if err = page.RenderInfo(authservice.InfoForm, authservice.Info{
-				Title:   "Password mismatch.",
-				Message: "New password and confirm password do not match.",
-			}, true); err != nil {
-				a.logger.PrintError(err.Error(), map[string]string{
-					"Source": sourcePr,
-				})
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
-			return
-		}
-	} else if newPass != "" || confirmPass == "" {
-		if currentPass == "" {
-			e := "Current password field cannot be empty"
-			a.logger.PrintError(e, map[string]string{
-				"Source": sourcePr,
-			})
-			if err := page.RenderInfo(authservice.InfoForm, authservice.Info{
-				Title:   "Empty field.",
-				Message: "Current password field cannot be empty.",
-			}, true); err != nil {
-				a.logger.PrintError(err.Error(), map[string]string{
-					"Source": sourcePr,
-				})
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
-			return
-		}
-	}
-
-	hashedPassword, err := a.hashPassword([]byte(newPass))
-	if err != nil {
-		e := helper.WrapError("Password hash error.", err)
-		a.logger.PrintError(e.Error(), map[string]string{
-			"Source": sourceUp,
-		})
-
-		if err = page.RenderInfo(authservice.InfoForm, authservice.Info{
-			Title:   "Something wrong happened.",
-			Message: "Kindly try again.",
-		}, true); err != nil {
+	newHashPassword, info := a.validatePassword(currentPass, newPass, confirmPass, user.Email)
+	if info != nil {
+		if err := page.RenderInfo(authservice.InfoForm, *info, true); err != nil {
 			a.logger.PrintError(err.Error(), map[string]string{
 				"Source": sourcePr,
 			})
@@ -149,10 +31,15 @@ func (a *Auth) UpdateProfileHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	updateUser.HashedPassword = hashedPassword
 
-	if username != "" {
+	var updateUser data.UpdateUser
+
+	if username != "" && username != user.Username {
 		updateUser.Username = &username
+	}
+
+	if newHashPassword != nil {
+		updateUser.HashedPassword = newHashPassword
 	}
 
 	if err := a.usermodel.Update(user.Id, updateUser); err != nil {
@@ -180,7 +67,7 @@ func (a *Auth) UpdateProfileHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err := page.RenderInfo(authservice.InfoForm, authservice.Info{
 		Title:   "Success.",
-		Message: "Your changes have been saved.",
+		Message: a.constructFeedbackMessage(updateUser),
 	}, false); err != nil {
 		a.logger.PrintError(err.Error(), map[string]string{
 			"Source": sourcePr,
@@ -188,9 +75,83 @@ func (a *Auth) UpdateProfileHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
-/* 
-func (a *Auth) validatePassword(currentPass, newPass, confirmPass, userEmail string, page *authservice.AuthService, w http.ResponseWriter) (bool, error) {
 
-	return true, nil
+func (a *Auth) constructFeedbackMessage(data data.UpdateUser) string {
+	switch {
+	case data.HashedPassword != nil && data.Username != nil:
+		return "Password and username changes have been saved."
+	case data.HashedPassword != nil:
+		return "Password changes have been saved."
+	case data.Username != nil:
+		return "Username changes have been saved."
+	default:
+		return "No new changes effected"
+	}
 }
- */
+
+func (a *Auth) validatePassword(currentPass, newPass, confirmPass, userEmail string) ([]byte, *authservice.Info) {
+	if currentPass == "" {
+		return nil, nil
+	} else {
+		userPass, err := a.usermodel.GetWithEmail(userEmail)
+		if err != nil {
+			a.logger.PrintError(err.Error(), map[string]string{
+				"Source": sourcePr,
+			})
+			return nil, &authservice.Info{
+				Title:   "Something wrong happened.",
+				Message: "Kindly try again.",
+			}
+		}
+
+		if err = a.compareHashedPassword(userPass.HashedPassword, []byte(currentPass)); err != nil {
+			a.logger.PrintError(err.Error(), map[string]string{
+				"Source": sourcePr,
+			})
+			return nil, &authservice.Info{
+				Title:   "Password mismatch.",
+				Message: "The current password you provided is not correct.",
+			}
+		}
+
+		if newPass == "" {
+			return nil, &authservice.Info{
+				Title:   "Empty field.",
+				Message: "The new password field cannot be empty.",
+			}
+		}
+
+		if confirmPass == "" {
+			return nil, &authservice.Info{
+				Title:   "Empty field.",
+				Message: "The confirm password field cannot be empty.",
+			}
+		}
+
+		if newPass != confirmPass {
+			return nil, &authservice.Info{
+				Title:   "Password mismatch.",
+				Message: "New password and confirm password do not match.",
+			}
+		}
+
+		newHashedPass, err := a.hashPassword([]byte(newPass))
+		if err != nil {
+			a.logger.PrintError(err.Error(), map[string]string{
+				"Source": sourcePr,
+			})
+			return nil, &authservice.Info{
+				Title:   "Something wrong happened.",
+				Message: "Kindly try again.",
+			}
+		}
+
+		if err = a.compareHashedPassword(newHashedPass, []byte(currentPass)); nil == err {
+			return nil, &authservice.Info{
+				Title:   "Not valid.",
+				Message: "Your new password is the same as your old password.",
+			}
+		}
+		return newHashedPass, nil
+	}
+}
